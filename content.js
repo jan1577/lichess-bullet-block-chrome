fcff6d11a4f40ed4c4fe437ca1bf8f312c059716
jalbers5 <jalbers5@smail.uni-koeln.de>
2022-07-26 18:12:17 +0200

    minor changes

--- content.js
+++ content.js
@@ -2,24 +2,17 @@
 let bullet1 = document.querySelector('[data-id="1+0"]');
 let bullet2 = document.querySelector('[data-id="2+1"]');
 
-// parent element that does not change when lobby chages
+// parent element that does not change when lobby/ quick pairing etc. are opened
 const parent_lobby = document.querySelector("#main-wrap > main");
 
 const mutationObserver = new MutationObserver(mutations => {
-    // added div: mutations[0].addedNodes[0]
-    console.log(mutations[0].addedNodes[0])
     // lobby is opended
     if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-real_time"){
-        console.log("lobby open");
+        ;
     }
     // quick pairing is opened
     else if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-pools"){
-        console.log(bullet1);
-        console.log(bullet2);
         remove_bullet_QP();
-    }
-    else{
-        console.log("Correspondence or playing games opened")
     }
 })
 
