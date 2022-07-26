cb6b3fce5add89387e42f217adf4aeeecb59f0ce
jalbers5 <jalbers5@smail.uni-koeln.de>
2022-07-26 18:31:55 +0200

    initial commit

--- content.js
+++ content.js
@@ -1,6 +1,3 @@
-// buttons in menu div
-let bullet1 = document.querySelector('[data-id="1+0"]');
-let bullet2 = document.querySelector('[data-id="2+1"]');
 
 // parent element that does not change when lobby/ quick pairing etc. are opened
 const parent_lobby = document.querySelector("#main-wrap > main");
@@ -20,10 +17,10 @@ mutationObserver.observe(parent_lobby, {childList: true})
 // mutationObserver.disconnect();
 
 function remove_bullet_QP(){
-    bullet1 = document.querySelector('[data-id="1+0"]');
-    bullet2 = document.querySelector('[data-id="2+1"]');
-    bullet1.remove();
-    bullet2.remove();
+    let _bullet1 = document.querySelector('[data-id="1+0"]');
+    let _bullet2 = document.querySelector('[data-id="2+1"]');
+    _bullet1.remove();
+    _bullet2.remove();
 }
 
 // remove bullet elements when first opening the page
