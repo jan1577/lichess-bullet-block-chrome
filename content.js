3a6ff7feb9d6fb6b7f045bfd2c6042f14074155d
jalbers5 <jalbers5@smail.uni-koeln.de>
2022-07-27 00:57:09 +0200

    remove New Opponent Button if Bullet game

--- content.js
+++ content.js
@@ -2,6 +2,7 @@
 // parent element that does not change when lobby/ quick pairing etc. are opened
 const parent_lobby = document.querySelector("#main-wrap > main");
 
+
 const mutationObserver = new MutationObserver(mutations => {
 
     // lobby is opended
@@ -13,9 +14,9 @@ const mutationObserver = new MutationObserver(mutations => {
     else if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-pools"){
         remove_bullet_QP();
     }
-})
+});
 
-mutationObserver.observe(parent_lobby, {childList: true})
+mutationObserver.observe(parent_lobby, {childList: true});
 
 
 function remove_bullet_QP(){
@@ -31,22 +32,23 @@ function lobby_open(){
     // add Mutation observer to <table class"hools__list"> to check if lobby is updated
     games_table = document.querySelector(
         "#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table"
-    )
+    );
 
     const mutationObserver_lobby = new MutationObserver(mutations => {
         // lobby is refreshed -> remove games once again
         remove_bullet_lobby();
-    })
+    });
     
     mutationObserver_lobby.observe(games_table, {childList: true, subtree: true})
     
 }
 
+
 function remove_bullet_lobby(){
 
     games_table = document.querySelector(
         "#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table"
-    )
+    );
     
     var tbody = games_table.getElementsByTagName('tbody')[0];
     var tableRow = tbody.getElementsByTagName('tr');
@@ -60,6 +62,7 @@ function remove_bullet_lobby(){
         }
     }
 }
+
 
 // remove bullet elements when first opening the page 
 // if statement to ensure that QP is open in case of reload
@@ -75,8 +78,29 @@ else if (document.querySelector("#main-wrap > main > div.lobby__app.lobby__app-r
 // slider to create game
 // change min value to 7 (3 min)
 if (document.querySelector("#modal-wrap > div > div.setup-content > div.time-mode-config.optional-config > div.time-choice.range > input")){
-    slider = document.querySelector(
+    let slider = document.querySelector(
         "#modal-wrap > div > div.setup-content > div.time-mode-config.optional-config > div.time-choice.range > input"
-    )
+    );
     slider.min = 7;
+}
+
+
+//  remove new opponent button in played games
+if (document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div")){
+    let new_opponent = document.querySelector(
+        "#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a"
+    );
+    console.log(new_opponent);
+    console.log(new_opponent.href);
+    let link = new_opponent.href.toString();
+    if (link.includes("1+0") || link.includes("2+1")){
+        console.log("bullet detected")
+        document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "none";
+        // document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols").stlye.display = "none";
+    }
+    else {
+        console.log(" no bullet")
+        // document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols").style.display = "block";
+        document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "block";
+    }
 }
