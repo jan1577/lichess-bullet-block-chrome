ad1978553e54169b0986e18cac7074d940e36fc5
jalbers5 <jalbers5@smail.uni-koeln.de>
2022-07-26 23:57:37 +0200

    remove bullet games from lobby

--- content.js
+++ content.js
@@ -3,10 +3,12 @@
 const parent_lobby = document.querySelector("#main-wrap > main");
 
 const mutationObserver = new MutationObserver(mutations => {
+
     // lobby is opended
     if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-real_time"){
-        ;
+        lobby_open();
     }
+
     // quick pairing is opened
     else if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-pools"){
         remove_bullet_QP();
@@ -14,7 +16,7 @@ const mutationObserver = new MutationObserver(mutations => {
 })
 
 mutationObserver.observe(parent_lobby, {childList: true})
-// mutationObserver.disconnect();
+
 
 function remove_bullet_QP(){
     let _bullet1 = document.querySelector('[data-id="1+0"]');
@@ -23,8 +25,52 @@ function remove_bullet_QP(){
     _bullet2.remove();
 }
 
-// remove bullet elements when first opening the page
-remove_bullet_QP();
+
+function lobby_open(){
+
+    // add Mutation observer to <table class"hools__list"> to check if lobby is updated
+    games_table = document.querySelector(
+        "#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table"
+    )
+
+    const mutationObserver_lobby = new MutationObserver(mutations => {
+        // lobby is refreshed -> remove games once again
+        remove_bullet_lobby();
+    })
+    
+    mutationObserver_lobby.observe(games_table, {childList: true, subtree: true})
+    
+}
+
+function remove_bullet_lobby(){
+
+    games_table = document.querySelector(
+        "#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table"
+    )
+    
+    var tbody = games_table.getElementsByTagName('tbody')[0];
+    var tableRow = tbody.getElementsByTagName('tr');
+
+    // loop through all games. if Bullet -> set display to none 
+    for (var t = 0; t < tableRow.length; t++){
+        var game_title = tableRow[t].title;
+        // use substring bullet to remove both bullet and ultrabullet
+        if (game_title.includes("Bullet")){
+            tableRow[t].style.display = "none";
+        }
+    }
+}
+
+// remove bullet elements when first opening the page 
+// if statement to ensure that QP is open in case of reload
+if (document.querySelector('[data-id="1+0"]')){
+    remove_bullet_QP();
+}
+// remove bullet games when page is refreshed while lobby is opened
+else if (document.querySelector("#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table > thead > tr > th:nth-child(2)")){
+    lobby_open();
+}
+
 
 
 
