04da492601de1305cf2cca10678ef0ce1f0a6d01
jalbers5 <jalbers5@smail.uni-koeln.de>
2022-08-02 18:14:44 +0200

    add option to block blitz games

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
+// first time use: set value to false
+chrome.storage.local.get(['block_blitz_storage'], function(result) {
+    if (result == null) {
+        console.log("undefined, setting storage")
+        chrome.storage.local.set({'block_blitz_storage': false});
+    }
+  });
+
 
 // parent element with 4 different tabs: Quick Pairing, Lobby, Correspondence, Games in play
 const parent_lobby = document.querySelector("#main-wrap > main");
@@ -12,7 +20,7 @@ const mutationObserver = new MutationObserver(mutations => {
 
     // quick pairing is opened
     else if (mutations[0].addedNodes[0].className == "lobby__app lobby__app-pools"){
-        remove_bullet_QP();
+        remove_elements_QP();
     }
 });
 // adding the observer to the parent_lobby
@@ -21,11 +29,28 @@ mutationObserver.observe(parent_lobby, {childList: true});
 /*
 * This function removes the two divs for Bullet Games in the Quick Pairing Menu.
 */
-function remove_bullet_QP(){
+function remove_elements_QP(){
+    // remove bullet
     let _bullet1 = document.querySelector('[data-id="1+0"]');
     let _bullet2 = document.querySelector('[data-id="2+1"]');
     _bullet1.remove();
     _bullet2.remove();
+
+    // get value for blitz from storage; if true, remove blitz values
+
+    chrome.storage.local.get(['block_blitz_storage'], function(result) {
+        
+        if (result['block_blitz_storage']){
+            let _blitz1 = document.querySelector('[data-id="3+0"]');
+            let _blitz2 = document.querySelector('[data-id="3+2"]');
+            let _blitz3 = document.querySelector('[data-id="5+0"]');
+            let _blitz4 = document.querySelector('[data-id="5+3"]');
+            _blitz1.remove();
+            _blitz2.remove();
+            _blitz3.remove();
+            _blitz4.remove();
+        };
+    });
 }
 
 /*
@@ -41,7 +66,7 @@ function lobby_open(){
 
     const mutationObserver_lobby = new MutationObserver(mutations => {
         // lobby is refreshed -> remove Nullet Games once again
-        remove_bullet_lobby();
+        remove_elements_lobby();
     });
     
     mutationObserver_lobby.observe(games_table, {childList: true, subtree: true})
@@ -49,7 +74,7 @@ function lobby_open(){
 }
 
 
-function remove_bullet_lobby(){
+function remove_elements_lobby(){
 
     games_table = document.querySelector(
         "#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table"
@@ -57,21 +82,35 @@ function remove_bullet_lobby(){
     
     var tbody = games_table.getElementsByTagName('tbody')[0];
     var tableRow = tbody.getElementsByTagName('tr');
-
-    // loop through all games. if Bullet -> set display to none 
-    for (var t = 0; t < tableRow.length; t++){
-        var game_title = tableRow[t].title;
-        // use substring bullet to remove both bullet and ultrabullet
-        if (game_title.includes("Bullet")){
-            tableRow[t].style.display = "none";
+    
+    chrome.storage.local.get(['block_blitz_storage'], function(result) {
+        if (result['block_blitz_storage']){
+            remove_elements_lobby(true)
+            // loop through all games. if Bullet -> set display to none 
+            for (var t = 0; t < tableRow.length; t++){
+                var game_title = tableRow[t].title;
+                // use substring bullet to remove both bullet, ultrabullet and blitz
+                if (game_title.includes("Bullet") || game_title.includes("Blitz")){
+                    tableRow[t].style.display = "none";
+                }
+            }
         }
-    }
+        else{
+            for (var t = 0; t < tableRow.length; t++){
+                var game_title = tableRow[t].title;
+                // use substring bullet to remove both bullet and ultrabullet
+                if (game_title.includes("Bullet")){
+                    tableRow[t].style.display = "none";
+                }
+            }
+        }
+    });
 }
 
 
 // Check if Quick Pairing is open when the page is loaded or refreshed.
 if (document.querySelector('[data-id="1+0"]')){
-    remove_bullet_QP();
+    remove_elements_QP();
 }
 // Check if the Lobby is open when the page is loaded or refreshed.
 else if (document.querySelector("#main-wrap > main > div.lobby__app.lobby__app-real_time > div.lobby__app__content.lreal_time > table > thead > tr > th:nth-child(2)")){
@@ -88,7 +127,14 @@ if (document.querySelector("#modal-wrap > div > div.setup-content > div.time-mod
     let slider = document.querySelector(
         "#modal-wrap > div > div.setup-content > div.time-mode-config.optional-config > div.time-choice.range > input"
     );
+
     slider.min = 7;
+
+    chrome.storage.local.get(['block_blitz_storage'], function(result) {
+        if (result['block_blitz_storage']){
+            slider.min = 14;
+        }
+    });
 }
 
 
@@ -104,12 +150,30 @@ if (document.querySelector("#main-wrap > main > div.round__app.variant-standard 
     // href for new game
     let link = new_opponent.href.toString();
 
-    if (link.includes("1+0") || link.includes("2+1")){
-        // if the current game is a Bullet Game, do not display the New Opponent button
-        document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "none";
-    }
-    else {
-        // if not a Bullet Game, display the New Opponent button
-        document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "block";
-    }
+    chrome.storage.local.get(['block_blitz_storage'], function(result) {
+        // check if blitz games are blocked
+        if (result['block_blitz_storage']){
+
+            let substrings = ["1+0", "2+1", "3+0", "3+2", "5+0", "5+3"]
+            if (substrings.some(str => link.includes(str))){
+                // if the current game is a Bullet or Blitz Game, do not display the New Opponent button
+                document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "none";
+            }
+            else {
+                // if not a Bullet Game, display the New Opponent button
+                document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "block";
+            }
+        // else: only block bullet games
+        } else {
+            let substrings = ["1+0", "2+1"]
+            if (substrings.some(str => link.includes(str))){
+                // if the current game is a Bullet Game, do not display the New Opponent button
+                document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "none";
+            }
+            else {
+                // if not a Bullet Game, display the New Opponent button
+                document.querySelector("#main-wrap > main > div.round__app.variant-standard > div.rcontrols > div > a:nth-child(2)").style.display = "block";
+            }
+        }
+    })
 }
---
+++ options.js
@@ -0,0 +1,40 @@
+// Saves options to chrome.storage
+function save_options() {
+    // check whihc box is checked and change value in storage
+    var block_blitz;
+    if(document.getElementById('block_blitz').checked) {
+        block_blitz = true;
+      }else if(document.getElementById('do_not_block_blitz').checked) {
+        block_blitz = false;
+      }
+    chrome.storage.local.set({
+      block_blitz_storage: block_blitz,
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
+  
+  // If options are opended, display the boxes so that the current option is checked
+  function restore_options() {
+    // Use default value block_blitz = false
+    chrome.storage.local.get(['block_blitz_storage'] , function(item) {
+        // if true
+    if (item['block_blitz_storage']){
+      document.getElementById('block_blitz').checked = true;
+      document.getElementById('do_not_block_blitz').checked = false;
+    }
+    else{
+        document.getElementById('block_blitz').checked = false;
+        document.getElementById('do_not_block_blitz').checked = true;
+    }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', restore_options);
+  document.getElementById('save').addEventListener('click',
+      save_options);
