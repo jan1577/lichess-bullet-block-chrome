38663eca12e007d5310cb3fc92eac6ae8310997a
Jan Albers <jalbers5@smail.uni-koeln.de>
2023-01-28 12:26:32 +0100

    add return statement to Array.from

--- puzzle.js
+++ puzzle.js
@@ -5,59 +5,60 @@ let links = document.getElementsByTagName('a'),
     holdarray = [];
 
 /* check all href for links to puzzle variants */ 
-Array.from(links, elem => {
-  if(elem.getAttribute('href') == "/storm"){
+Array.from(links, link => {
+  if(link.getAttribute('href') == "/storm"){
     // get current option
     chrome.storage.local.get(['block_puzzle_storm'], function(result) {
         // check if puzzle is blocked
         // if it's a "button" -> remove it
         if (result['block_puzzle_storm']){
-            if(elem.parentElement.role == "group" || elem.className == "storm-play-again button"){
-                elem.style.display = 'none';
+            if(link.parentElement.role == "group" || link.className == "storm-play-again button"){
+                link.style.display = 'none';
             }
             // if it's just "text", remove href and styling, but still display
             else{
-                removeHref(elem);
+                removeHref(link);
             }
         }  
     });
   }
-  else if (elem.getAttribute('href') == "/racer"){
+  else if (link.getAttribute('href') == "/racer"){
     // get current option
     chrome.storage.local.get(['block_puzzle_racer'], function(result) {
         // check puzzle racer is blocked
         if (result['block_puzzle_racer']){
-            if (elem.parentElement.role == "group"){
-                elem.style.display = 'none';
+            if (link.parentElement.role == "group"){
+                link.style.display = 'none';
             }   
             else{
-                removeHref(elem);
+                removeHref(link);
                 // remove small icon next to span on left menu bar
-                if (elem.children[1]){
-                    elem.children[1].remove();
+                if (link.children[1]){
+                    link.children[1].remove();
                 }
             } 
         }
     });
   }
-  else if (elem.getAttribute('href') == "/streak"){
+  else if (link.getAttribute('href') == "/streak"){
     // get current option
     chrome.storage.local.get(['block_puzzle_streak'], function(result) {
         // check if puzzle streak blocked
         if (result['block_puzzle_streak']){
-            if (elem.parentElement.role == "group"){
-                elem.style.display = 'none';
+            if (link.parentElement.role == "group"){
+                link.style.display = 'none';
             }
             else{
-                removeHref(elem);
+                removeHref(link);
                 // remove small icon next to span on left menu bar
-                if (elem.children[1]){
-                    elem.children[1].remove();
+                if (link.children[1]){
+                    link.children[1].remove();
                 }
             }
         }
     });
   }
+  return
 });
 
 /* This function is used to remove the href attribute, and styles for elements that should be
